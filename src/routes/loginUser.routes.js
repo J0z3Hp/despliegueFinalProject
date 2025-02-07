c3b804f7b831cef560a086ce7863547c7da64cbf
J0z3Hp <joseluismarinocuadros@gmail.com>
2025-02-06 21:08:42 -0500

    [fixed] arreglando problemas para el despliegue

--- src/routes/loginUser.routes.js
+++
@@ -1,8 +0,0 @@
-import { loginUser } from "../services/loginUserService.js";
-import express from "express";
-
-
-// Configramos el router de express e importamos la dependencia para poder hacerlo
-export const loginUserRouter = express.Router();
-
-loginUserRouter.post("/", loginUser)
